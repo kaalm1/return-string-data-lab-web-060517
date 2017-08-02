92e8ecc5e2bd583ae6ee552b3b61cce083b367d4
kaalm1 <kaalm1@gmail.com>
2017-08-02 13:15:42 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,43 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    Product.create(product_params)
+    redirect_to products_path
+  end
+
+  def inventory
+    @product = Product.find(params[:id])
+    if @product.inventory != 0
+      render text: "true"
+    else
+      render text: "false"
+    end
+  end
+
+  def description
+    @product = Product.find(params[:id])
+    render text: "#{@product.description}"
+  end
+
+  def info
+    @product = Product.find(params[:id])
+    if @product.inventory != 0
+       status = "Available"
+    else
+      status = "Sold Out"
+    end
+    render text: "#{@product.description} #{status}"
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description,:inventory,:price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
+  get 'products/:id/inventory', to: 'products#inventory', as: "product_inventory"
+  get 'products/:id/description', to: 'products#description', as: "product_description"
+  post 'products/:id/info', to: 'products#info', as: 'product_info'
   resources :orders
   resources :invoices
+  resources :products
 end
